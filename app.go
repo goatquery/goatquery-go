04c3cdd81861b1f1085c9917186b55216549f513
Jamess-Lucass <jameslucas1210@gmail.com>
2023-09-04 10:44:49 +0100

    fix: Parse to correct column name

--- app.go
+++ app.go
@@ -2,12 +2,14 @@ package goatquery
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 
 	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 )
 
-func Apply(db *gorm.DB, query Query, maxTop *int, searchFunc func(db *gorm.DB, searchTerm string) *gorm.DB) (*gorm.DB, *int64, error) {
+func Apply(db *gorm.DB, query Query, maxTop *int, searchFunc func(db *gorm.DB, searchTerm string) *gorm.DB, model interface{}) (*gorm.DB, *int64, error) {
 	if maxTop != nil && query.Top > *maxTop {
 		return nil, nil, fmt.Errorf("The value supplied for the query parameter 'Top' was greater than the maximum top allowed for this resource")
 	}
@@ -21,6 +23,13 @@ func Apply(db *gorm.DB, query Query, maxTop *int, searchFunc func(db *gorm.DB, s
 	if query.Filter != "" {
 		filters := splitString(query.Filter)
 		where := strings.Builder{}
+
+		namer := db.Statement.NamingStrategy
+
+		v := reflect.ValueOf(model)
+		modelType := reflect.Indirect(v).Type().Elem()
+
+		tableName := namer.TableName(modelType.Name())
 
 		for i, filter := range filters {
 			opts := splitStringByWhitespace(strings.TrimSpace(filter))
@@ -37,6 +46,8 @@ func Apply(db *gorm.DB, query Query, maxTop *int, searchFunc func(db *gorm.DB, s
 			property := opts[0]
 			operand := opts[1]
 			value := opts[2]
+
+			property = GetGormColumnNameByJsonTag(namer, tableName, modelType, property)
 
 			if strings.EqualFold(operand, "contains") {
 				valueWithoutQuotes := getValueBetweenQuotes(value)
@@ -85,4 +96,21 @@ func Apply(db *gorm.DB, query Query, maxTop *int, searchFunc func(db *gorm.DB, s
 	}
 
 	return db, nil, nil
+}
+
+func GetGormColumnNameByJsonTag(namer schema.Namer, tableName string, t reflect.Type, property string) string {
+	for i := 0; i < t.NumField(); i++ {
+		f := t.Field(i)
+		v := strings.Split(f.Tag.Get("json"), ",")[0] // use split to ignore tag "options" like omitempty, etc.
+		if v == property {
+			settings := schema.ParseTagSetting(f.Tag.Get("gorm"), ";")
+			if settings["COLUMN"] != "" {
+				return settings["COLUMN"]
+			}
+
+			return namer.ColumnName(tableName, f.Name)
+		}
+	}
+
+	return property
 }
